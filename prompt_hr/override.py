ab923d5f22a431345e523a440a4aa4a689da0bb5
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-04-07 17:37:08 +0530

    Update  job applicant status based on interview

--- prompt_hr/override.py
+++ prompt_hr/override.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe import _
 from hrms.hr.doctype.interview.interview import Interview, get_recipients
-
+from hrms.hr.doctype.job_offer.job_offer import JobOffer
 
 
 
@@ -112,4 +112,10 @@ class CustomInterview(Interview):
 
         frappe.msgprint(_("Interview Rescheduled successfully"), indicator="green")
   
-  
+
+class CustomJobOffer(JobOffer):
+    def on_change(self):
+        """Override the on_change method to stop calling update_job_applicant which updates the status of the job applicant.
+        This is to prevent the status from being updated when the job offer status is set to either Accepted or Rejected.
+        """
+        pass
