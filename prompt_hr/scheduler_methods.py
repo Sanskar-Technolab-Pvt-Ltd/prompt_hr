450b4a11fdfcf40d8c32a516d5741c3d9ecd744b
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-04-10 19:10:05 +0530

    Probation Feedback notification for reporting manager

--- prompt_hr/scheduler_methods.py
+++ prompt_hr/scheduler_methods.py
@@ -30,7 +30,6 @@ def create_probation_feedback_form():
                     
                     create_only_one = True if not first_feedback_form_id and not second_feedback_form_id else False
                     
-                    print(f" \n\n create only one {create_only_one} {second_feedback_form_id} \n\n")
                     
                     if emp_joining_date:
                         date_difference= date_diff(today(), emp_joining_date)
@@ -69,15 +68,28 @@ def create_probation_feedback_form():
                                 employee_doc.save(ignore_permissions=True)
                                 frappe.db.commit()
                             else:
-                                remarks_added = frappe.db.exists("Probation Feedback Prompt", {"parenttype": "Probation Feedback Form", "parent": first_feedback_form_id, "rating": ["is", "set"]})
+                                remarks_added = frappe.db.exists("Probation Feedback Prompt", {"parenttype": "Probation Feedback Form", "parent": first_feedback_form_id, "rating": ["not in", ["0", ""]]}, "name")
+                                
                                 
                                 if not remarks_added:
-                                    print(f"Send Mail")
-                        
+                                    reporting_manager_emp_id = frappe.db.get_value("Probation Feedback Form", first_feedback_form_id, "reporting_manager") or None
+                                    
+                                    if not reporting_manager_emp_id:
+                                        reporting_manager_emp_id = frappe.db.get_value("Employee", employee.get("name"), "reports_to") or None
+                                        
+                                
+                                    if reporting_manager_emp_id:
+                                        
+                                        reporting_manager_user_id = frappe.db.get_value("Employee", reporting_manager_emp_id, "user_id") or None
+                                        
+                                        
+                                        if reporting_manager_user_id:
+                                            reporting_manager_email = frappe.db.get_value("User", reporting_manager_user_id, "email")
+                                            if reporting_manager_email:
+                                                send_reminder_mail_to_reporting_manager(reporting_manager_email, reporting_manager_user_id, first_feedback_form_id, employee.get("employee_name"))
+                                                
                         if second_feedback_days <= date_difference:
-                            print(f" \n\n creating Second Feedback Form \n\n")
                             if not second_feedback_form_id and not create_only_one:
-                                print(f" \n\n creating Second Feedback Form \n\n")
                                 
                                 
                                 employee_doc = frappe.get_doc("Employee", employee.get("name"))
@@ -105,17 +117,72 @@ def create_probation_feedback_form():
                                             "frequency": "60 Days"
                                         })
                                 
-                                
                                 second_probation_form.insert(ignore_permissions=True)
                                 employee_doc.custom_second_probation_feedback = second_probation_form.name
                                 employee_doc.save(ignore_permissions=True)
+                                
+                                
+                                
+                                
                                 frappe.db.commit()
                             elif second_feedback_form_id:
-                                    remarks_added = frappe.db.exists("Probation Feedback Prompt", {"parenttype": "Probation Feedback Form", "parent": second_feedback_form_id, "rating": ["is", "set"]})
-                                    
+                                    print(f" \n\n Second Feedback Form already created \n\n")
+
+                                    remarks_added = frappe.db.exists("Probation Feedback Prompt", {"parenttype": "Probation Feedback Form", "parent": second_feedback_form_id, "rating": ["not in", ['0', '']]})
                                     if not remarks_added:
-                                        print(f"Send Mail")
+                                        reporting_manager_emp_id = frappe.db.get_value("Probation Feedback Form", second_feedback_form_id, "reporting_manager") or None
+                                        if not reporting_manager_emp_id:
+                                            reporting_manager_emp_id = frappe.db.get_value("Employee", employee.get("name"), "reports_to") or None
+
+                                        if reporting_manager_emp_id:
+                                            reporting_manager_user_id = frappe.db.get_value("Employee", reporting_manager_emp_id, "user_id") or None
+                                            if reporting_manager_user_id:
+                                                reporting_manager_email = frappe.db.get_value("User", reporting_manager_user_id, "email")
+                                                if reporting_manager_email:
+                                                    send_reminder_mail_to_reporting_manager(reporting_manager_email, reporting_manager_user_id, second_feedback_form_id, employee.get("employee_name"))
+                                                    
     except Exception as e:
         frappe.log_error("Error while creating probation feedback form", frappe.get_traceback())
 
 
+def send_reminder_mail_to_reporting_manager(reporting_manager_email, reporting_manager_user_id,probation_feedback_form_id, employee_id):
+    """Method to send a reminder email to the reporting manager"""
+    
+    try:
+        notification = frappe.get_doc({
+                "doctype": "Notification Log",
+                "subject": "Add Remarks to Feedback Form",
+                "for_user": reporting_manager_user_id,
+                "type": "Energy Point",
+                "document_type": "Probation Feedback Form",
+                "document_name": probation_feedback_form_id,
+            })
+        notification.insert(ignore_permissions=True) 
+        
+        frappe.sendmail(
+            recipients=[reporting_manager_email,],
+            subject="Feedback Form Reminder",
+            content=f"Reminder: Add Remarks to Feedback Form {probation_feedback_form_id} for {employee_id}",
+            # now = True
+            )
+        frappe.db.commit()
+    except Exception as e:
+        frappe.log_error("Error while sending second feedback mail", frappe.get_traceback())
+        print(f" \n\n Second Feedback Mail Failed: {frappe.get_traceback()} \n\n")
+
+
+
+
+@frappe.whitelist()
+def create_confirmation_evaluation_form():
+    try:
+        employees_list = frappe.db.get_all("Employee", {"status": "Active", "company": "Prompt Equipments PVT LTD", "custom_probation_status": "Pending"}, "name")
+        
+        if employees_list:
+            for employee_id in employees_list:
+                probation_days = frappe.db.get_value("Employee", employee_id.get("name"), "custom_probation_period")
+                
+                if probation_days:
+                    pass
+    except Exception as e:
+        frappe.log_error("Error while creating confirmation evaluation form", frappe.get_traceback())
