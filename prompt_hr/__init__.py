818261316a7ba5bbbdad480d48e02615a0d0ed07
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-04-22 19:46:53 +0530

    fix: init file

--- prompt_hr/__init__.py
+++ prompt_hr/__init__.py
@@ -1,41 +1,5 @@
 __version__ = "0.0.1"
 
-import frappe
-from typing import List
-import hrms.hr.doctype.interview_feedback.interview_feedback as interview_feedback_module
-
-@frappe.whitelist()
-def custom_get_applicable_interviewers(interview: str) -> List[str]:
-    interviewers = frappe.get_all(
-        "Interview Detail",
-        filters={"parent": interview},
-        fields=["custom_interviewer_employee", "custom_interviewer_name", "custom_is_confirm", "name"]
-    )
-    external_interviewers = frappe.get_all(
-        "External Interviewer",
-        filters={"parent": interview},
-        fields=["user", "user_name", "is_confirm", "name"]
-    )
-    users = []
-    for interviewer in interviewers:
-        if interviewer.custom_interviewer_employee:
-            employee = frappe.get_doc("Employee", interviewer.custom_interviewer_employee)
-            if employee.user_id:
-                user = frappe.get_doc("User", employee.user_id)
-                if user and user.enabled and user.name not in users:
-                    users.append(user.name)
-    for interviewer in external_interviewers:
-        if interviewer.get("user"):
-            supplier = frappe.get_doc("Supplier", interviewer.user)
-            if supplier.custom_user:
-                user = frappe.get_doc("User", supplier.custom_user)
-                if user and user.enabled and user.name not in users:
-                    users.append(user.name)
-
-    return users
-
-# Override original function
-interview_feedback_module.get_applicable_interviewers = custom_get_applicable_interviewers
 
 import frappe
 from typing import List
