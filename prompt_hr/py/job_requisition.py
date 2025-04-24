26791bc553c2312b455e9f402d7cc12a7133826d
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-04-24 19:39:13 +0530

    Started indifoss probation and confirmation flow

--- prompt_hr/py/job_requisition.py
+++ prompt_hr/py/job_requisition.py
@@ -2,7 +2,6 @@ import frappe
 
 @frappe.whitelist()
 def on_update(doc, method):
-    print(f"\n\n both are getting called\n\n")
     if doc.custom_target_hiring_duration:
         if doc.custom_target_hiring_duration == "Custom Date":
             doc.custom_target_hiring_date = doc.expected_by if doc.expected_by else frappe.utils.nowdate()
@@ -13,6 +12,8 @@ def on_update(doc, method):
 
 @frappe.whitelist()
 def add_or_update_custom_last_updated_by(doc, method):
+    """ Method to set custom_last_updated_by_employee and custom_employee_name fields in Job Requisition to recognize the last user who modified the document.
+    """
     try:
         if doc.modified_by:
             employee_id = frappe.get_value("Employee", {"user_id": doc.modified_by}, "name")
@@ -28,3 +29,19 @@ def add_or_update_custom_last_updated_by(doc, method):
     except Exception as e:
         frappe.log_error(f"Error in add_or_update_custom_last_updated_by: {str(e)}", "Job Requisition")
     
+
+def set_requested_by(doc, event):
+    """ Method to set requested_by field in Job Requisition if not set
+    """
+    try:
+        if not doc.requested_by:
+            user = frappe.session.user
+            employee_id = frappe.get_value("Employee", {"user_id": user}, "name")
+            
+            if employee_id:
+                employee_name = frappe.get_value("Employee", employee_id, "employee_name")
+                doc.requested_by = employee_id
+                doc.requested_by_name = employee_name
+# 
+    except Exception as e:
+        frappe.log_error(f"Error in set_requested_by Job Requisition", frappe.get_traceback())
