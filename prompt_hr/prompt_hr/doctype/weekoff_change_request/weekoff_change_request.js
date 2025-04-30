929cc29ab91d232455593617e4ca2d74bc5a61ff
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-04-30 19:21:49 +0530

    WeekOff Change Request Flow Started

--- prompt_hr/prompt_hr/doctype/weekoff_change_request/weekoff_change_request.js
+++ prompt_hr/prompt_hr/doctype/weekoff_change_request/weekoff_change_request.js
@@ -1,8 +1,70 @@
 // Copyright (c) 2025, Jignasha Chavda and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("WeekOff Change Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("WeekOff Change Request", {
+	refresh(frm) {
 
-// 	},
-// });
+        const user = frappe.session.user
+        frappe.call({
+            "method": "prompt_hr.prompt_hr.doctype.weekoff_change_request.weekoff_change_request.check_user_is_reporting_manager",
+            "args": {
+                user_id: user,
+                requesting_employee_id: frm.doc.employee
+            },
+            callback: function (r) {
+                if (!r.message.error) {
+                    if (r.message.is_rh) {
+                        frm.set_df_property("status", "hidden", 0)
+                    }
+                } else if (r.message.error) {
+                    frappe.throw(r.message.message)
+                }
+            }
+        })
+
+    },
+    
+});
+frappe.ui.form.on('WeekOff Request Details', {
+    existing_weekoff_date: function (frm, cdt, cdn) {
+
+        let child = locals[cdt][cdn];
+        if (child.existing_weekoff_date) {
+            console.log("Hello")
+            
+            frappe.call({
+                method: "prompt_hr.prompt_hr.doctype.weekoff_change_request.weekoff_change_request.check_existing_date",
+                args: {
+                    employee_id: frm.doc.employee,
+                    existing_date: child.existing_weekoff_date
+                },
+                callback: function (r) {
+                    
+                    if (!r.message.error) {
+                        if (r.message.exists) {
+
+                            frappe.model.set_value(cdt, cdn, 'existing_weekoff', r.message.day);
+                        } else {
+                            frappe.model.set_value(cdt, cdn, 'existing_weekoff', '');
+                            frappe.throw(`Date ${child.existing_weekoff_date} does not exist in Holiday List`)
+                        
+                        }
+                    } else if (r.message.error) {
+                        frappe.throw(`Error While Verifying Existing Date. ${r.message.message}`)
+                    }
+                }
+            });
+        }
+    },
+
+    new_weekoff_date: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.new_weekoff_date) {
+            let date_obj = new Date(row.new_weekoff_date);
+            let day_name = date_obj.toLocaleString('en-US', { weekday: 'long' });
+            row.new_weekoff = day_name;
+            frm.refresh_field('weekoff_details'); 
+        }
+        
+    }
+});
