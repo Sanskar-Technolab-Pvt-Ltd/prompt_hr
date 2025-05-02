27d5dac2559e039008ad98bbc1ddfc8f9a99e125
JignasaChavda <jignasha@sanskartechnolab.com>
2025-05-02 12:24:36 +0530

    updated meeting request form fields

--- prompt_hr/prompt_hr/doctype/meeting_request/meeting_request.js
+++ prompt_hr/prompt_hr/doctype/meeting_request/meeting_request.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Jignasha Chavda and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Meeting Room Booking Request", {
+// frappe.ui.form.on("Meeting Request", {
 // 	refresh(frm) {
 
 // 	},
