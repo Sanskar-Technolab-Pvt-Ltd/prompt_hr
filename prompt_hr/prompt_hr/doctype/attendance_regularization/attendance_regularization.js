00a4e54192f3cd70d01e50c8b0b3b4297350675f
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-05-05 19:28:43 +0530

    Attendance Regularization Flow Started

--- prompt_hr/prompt_hr/doctype/attendance_regularization/attendance_regularization.js
+++ prompt_hr/prompt_hr/doctype/attendance_regularization/attendance_regularization.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2025, Jignasha Chavda and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Attendance Regularization", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Attendance Regularization", {
+	refresh(frm) {
+        const user = frappe.session.user
+        if (frm.doc.employee) {
+            frappe.call({
+                "method": "prompt_hr.py.utils.check_user_is_reporting_manager",
+                "args": {
+                    user_id: user,
+                    requesting_employee_id: frm.doc.employee
+                },
+                callback: function (r) {
+                    if (!r.message.error) {
+                        if (r.message.is_rh) {
+                            frm.set_df_property("status", "hidden", 0)
+                        }
+                    } else if (r.message.error) {
+                        frappe.throw(r.message.message)
+                    }
+                }
+            })
+        }
+	},
+});
