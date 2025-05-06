0cdf01c8bff79ef4c743d397d5c4912dea6894d8
malav8 <malav.sanskartechnolab.com>
2025-05-06 20:06:35 +0530

    Added Process FnF Button and Populated Leave Application Table in Payroll Entry.

--- prompt_hr/hooks.py
+++ prompt_hr/hooks.py
@@ -59,7 +59,7 @@ doctype_js = {
     "Interview Feedback": "public/js/interview_feedback.js",
     "Interview Round": "public/js/interview_round.js",
     "Attendance": "public/js/attendance.js",
-    "Attendance Request": "public/js/attendance_request.js"
+    "Attendance Request": "public/js/attendance_request.js",
     "Payroll Entry": "public/js/payroll_entry.js",
 
 }
@@ -212,7 +212,7 @@ doc_events = {
     # },
     "Attendance Request": {
         "after_insert": "prompt_hr.py.attendance_request.after_insert"
-    }
+    },
     "Payroll Entry": {
         "before_save": "prompt_hr.py.payroll_entry.before_save",
     },
