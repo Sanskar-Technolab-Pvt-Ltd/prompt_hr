00b5620101ee0e6cc14f24c361aa2948ed6fda92
malav8 <malav.sanskartechnolab.com>
2025-05-08 16:46:13 +0530

    Added LOP Days in Addition of Payment Days in Salary Slip Doctype.

--- prompt_hr/__init__.py
+++ prompt_hr/__init__.py
@@ -12,6 +12,8 @@ import hrms.hr.report.employee_leave_balance.employee_leave_balance as leave_bal
 from frappe.model import workflow
 from prompt_hr.overrides.workflow_override import custom_get_transitions, custom_has_approval_access
 from prompt_hr.py.leave_application import custom_get_number_of_leave_days, custom_update_previous_leave_allocation, custom_check_effective_date, custom_get_leave_details, custom_get_allocated_and_expired_leaves
+from hrms.payroll.doctype.payroll_entry import payroll_entry 
+from prompt_hr.py.salary_slip_overriden_methods import custom_create_salary_slips_for_employees
 
 @frappe.whitelist()
 def custom_get_applicable_interviewers(interview: str) -> List[str]:
@@ -63,3 +65,4 @@ hrms.hr.utils.update_previous_leave_allocation = custom_update_previous_leave_al
 leave_application_module.get_number_of_leave_days = custom_get_number_of_leave_days
 leave_application_module.get_leave_details = custom_get_leave_details
 leave_balance_report.get_allocated_and_expired_leaves = custom_get_allocated_and_expired_leaves 
+payroll_entry.create_salary_slips_for_employees = custom_create_salary_slips_for_employees
