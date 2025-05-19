d8a5b4b761315c7d8ec5f240f6a67dc30fbb93a1
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-05-19 19:25:45 +0530

    fix:Modify Print Formats

--- prompt_hr/prompt_hr/doctype/employee_gratuity/employee_gratuity.js
+++ prompt_hr/prompt_hr/doctype/employee_gratuity/employee_gratuity.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2025, Jignasha Chavda and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Gratuity", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Employee Gratuity", {
+    employee(frm) {
+        frm.set_value("total_working_year", null);
+        if (frm.doc.employee) {
+            setTimeout(() => {
+                // IF Date of Leaving is not set, then throw error
+                if (!frm.doc.date_of_leaving) {
+                    frappe.throw(__("Relieving Date is mandatory for gratuity calculation"));
+                } else {
+                    const diff = frappe.datetime.get_diff(frm.doc.date_of_leaving, frm.doc.date_of_joining);
+                    frm.set_value("total_working_year", diff / 365); // Set total working years
+                }
+            }, 300);
+        }
+    }
+});
