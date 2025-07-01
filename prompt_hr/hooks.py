f02c7d25659e9a1084b2553c3736d87c9fed1965
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-01 18:35:57 +0530

    Changes in Leave Workflow

--- prompt_hr/hooks.py
+++ prompt_hr/hooks.py
@@ -475,7 +475,7 @@ fixtures = [
     #     "dt":"Role", "filters": [["name", "in", ["Job Requisition", "Head of Department", "Managing Director"]]]
     # },
     # {
-    #     "dt":"Workflow", "filters": [["name", "in", ["Job Requisition","Expense Claim", "Compensatory Leave Request", "Leave Application", "Loan Application"]]]
+    #     "dt":"Workflow", "filters": [["name", "in", ["Job Requisition", "Expense Claim", "Compensatory Leave Request", "Leave Application", "Loan Application", "Travel Request"]]]
     # },
     # {
     #     "dt":"Workflow State", "filters": [["name", "in", ["Approved by HOD", "Pending", "Rejected by HOD", "Approved by Director", "Rejected by Director", "Cancelled", "On-Hold", "Filled", "Confirmed", "Approved by HR", "Rejected by HR", "Approved by BU Head", "Rejected by BU Head", "Extension Approved", "Extension Confirmed", "Extension Rejected", "Extension Requested"]]]
