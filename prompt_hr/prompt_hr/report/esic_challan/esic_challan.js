7f847b471751321da054a656b8e91827c3caf671
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-21 14:46:25 +0530

    feat: Add ESIC Challan/PF-ECR Challan notification to Accounting Team via report button
    Added 'Notify Accounting Team' button for HR roles on ESIC Challan/PF-ECR Challan report. Sends email using Notification DocType to users with 'Accounts User' or 'Accounts Manager' roles. Throws error if no recipients. Uses 'PF-ECR and ESIC Challan Notification' template with report link.

--- prompt_hr/prompt_hr/report/esic_challan/esic_challan.js
+++ prompt_hr/prompt_hr/report/esic_challan/esic_challan.js
@@ -2,7 +2,36 @@
 // For license information, please see license.txt
 
 frappe.query_reports["ESIC_Challan"] = {
-	"filters": [
+    "filters": [],
 
-	]
+    onload: function(report) {
+        const roles = frappe.user_roles || [];
+
+        // Button 1: Notify Accountify Team
+        if (roles.includes("HR User") || roles.includes("HR Manager")) {
+            report.page.add_inner_button("Notify Accountify Team", () => {
+                frappe.call({
+                    method: "prompt_hr.py.accounting_team_notifications.send_esic_challan_notification",
+                    args: {
+                        report_name: "ESIC Challan"
+                    },
+                    callback: function(r) {
+                        if (r.message === "success") {
+                            frappe.msgprint("Notification sent to the Accounting team Succesfully.");
+                        }
+                    }
+                });
+            }).removeClass("btn-default").addClass("btn-primary");
+        }
+
+        // Button 2: Create Payment Entry
+        if (roles.includes("Accounts User") || roles.includes("Accounts Manager")) {
+            report.page.add_inner_button("Create Payment Entry", () => {
+                frappe.new_doc("Payment Entry", {
+                    payment_type: "Pay",
+                    party_type: "Employee",
+                });
+            }).removeClass("btn-default").addClass("btn-primary");
+        }
+    }
 };
