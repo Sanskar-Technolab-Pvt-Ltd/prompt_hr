6a77d0c9f27c739029423860533d177aa9e4cf64
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-23 10:55:18 +0530

    feat: set Effective To as Leave Period end date if assigning leave from joining date

--- prompt_hr/public/js/leave_policy_assignment.js
+++ prompt_hr/public/js/leave_policy_assignment.js
@@ -1,3 +1,4 @@
+frappe.ui.form.off("Leave Policy Assignment", "set_effective_date")
 frappe.ui.form.on("Leave Policy Assignment", {
   // Set default filter on refresh based on company
   refresh: function (frm) {
@@ -53,4 +54,59 @@ frappe.ui.form.on("Leave Policy Assignment", {
       });
     }
   },
+
+  set_effective_date: function (frm) {
+		if (frm.doc.assignment_based_on == "Leave Period" && frm.doc.leave_period) {
+			frappe.model.with_doc("Leave Period", frm.doc.leave_period, function () {
+				let from_date = frappe.model.get_value(
+					"Leave Period",
+					frm.doc.leave_period,
+					"from_date",
+				);
+				let to_date = frappe.model.get_value(
+					"Leave Period",
+					frm.doc.leave_period,
+					"to_date",
+				);
+				frm.set_value("effective_from", from_date);
+				frm.set_value("effective_to", to_date);
+			});
+		} else if (frm.doc.assignment_based_on == "Joining Date" && frm.doc.employee) {
+			frappe.model.with_doc("Employee", frm.doc.employee, function () {
+				let from_date = frappe.model.get_value(
+					"Employee",
+					frm.doc.employee,
+					"date_of_joining",
+				);
+				frm.set_value("effective_from", from_date);
+				frappe.call({
+          method: "frappe.client.get_list",
+          args: {
+            doctype: "Leave Period",
+            filters: {
+              from_date: ["<=", frm.doc.effective_from],
+              to_date: [">=", frm.doc.effective_from],
+              is_active: 1,
+            },
+            fields: ["to_date"],
+            limit_page_length: 1
+          },
+          callback: function (r) {
+            if (r.message && r.message.length > 0) {
+              //? SET "EFFECTIVE_TO" TO "TO_DATE" OF MATCHING LEAVE PERIOD
+              frm.set_value("effective_to", r.message[0].to_date);
+            } else {
+              //? IF NOT FOUND, SET "EFFECTIVE_TO" TO 31ST DECEMBER OF THAT YEAR
+              const from_dt = frappe.datetime.str_to_obj(frm.doc.effective_from);
+              const dec_31_dt = new Date(from_dt.getFullYear(), 11, 31);  // Month 11 = December
+              const datetime_str = frappe.datetime.obj_to_str(dec_31_dt);
+              frm.set_value("effective_to", datetime_str);
+            }
+          }
+        });
+
+			});
+		}
+		frm.refresh();
+	},
 });
