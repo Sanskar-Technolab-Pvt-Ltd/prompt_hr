0481038e8a04212309ad33c4e74f7aaf120d7d9f
JignasaChavda <jignasha@sanskartechnolab.com>
2025-07-27 14:32:59 +0530

    updated logic for income tax calculation

--- prompt_hr/overrides/salary_slip_override.py
+++ prompt_hr/overrides/salary_slip_override.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from frappe.utils import flt
+from frappe.utils import ceil, flt
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
 
 # ? CustomSalarySlip enhances SalarySlip by including:
@@ -172,3 +172,155 @@ class CustomSalarySlip(SalarySlip):
             self.compute_year_to_date()
             self.compute_month_to_date()
             self.compute_component_wise_year_to_date()
+        
+    def compute_annual_deductions_before_tax_calculation(self):
+        tax_slab = frappe.db.get_value('Salary Structure Assignment', self.custom_salary_structure_assignment, 'income_tax_slab')
+        is_new_tax_slab = frappe.db.get_value('Income Tax Slab', tax_slab, 'custom_is_new_regime')
+
+        prev_period_exempted_amount = 0
+        current_period_exempted_amount = 0
+        future_period_exempted_amount = 0
+
+        # Previous period exempted amount (applies only for exempted components)
+        prev_period_exempted_amount = self.get_salary_slip_details(
+            self.payroll_period.start_date,
+            self.start_date,
+            parentfield="deductions",
+            exempted_from_income_tax=1,
+        )
+
+        # Current period exempted amount
+        for d in self.get("deductions"):
+            if d.exempted_from_income_tax:
+                if is_new_tax_slab:
+                    # In new slab, check if the component should NOT be exempt
+                    not_exempt_for_new_slab = frappe.db.get_value('Salary Component', d.salary_component, 'custom_do_not_exempt_for_new_tax_slab')
+                    if not not_exempt_for_new_slab:
+                        current_period_exempted_amount += d.amount
+                else:
+                    # In old slab, exempt directly
+                    current_period_exempted_amount += d.amount
+
+        # Future period exempted amount
+        for deduction in self._salary_structure_doc.get("deductions"):
+            if deduction.exempted_from_income_tax:
+                if is_new_tax_slab:
+                    not_exempt_for_new_slab = frappe.db.get_value('Salary Component', deduction.salary_component, 'custom_do_not_exempt_for_new_tax_slab')
+                    if not not_exempt_for_new_slab:
+                        if deduction.amount_based_on_formula:
+                            for sub_period in range(1, ceil(self.remaining_sub_periods)):
+                                future_period_exempted_amount += self.get_amount_from_formula(deduction, sub_period)
+                        else:
+                            future_period_exempted_amount += deduction.amount * (ceil(self.remaining_sub_periods) - 1)
+                else:
+                    # In old slab, exempt directly
+                    if deduction.amount_based_on_formula:
+                        for sub_period in range(1, ceil(self.remaining_sub_periods)):
+                            future_period_exempted_amount += self.get_amount_from_formula(deduction, sub_period)
+                    else:
+                        future_period_exempted_amount += deduction.amount * (ceil(self.remaining_sub_periods) - 1)
+
+        return (
+            prev_period_exempted_amount + current_period_exempted_amount + future_period_exempted_amount
+        ) or 0
+
+
+    def get_taxable_earnings(self, allow_tax_exemption=False, based_on_payment_days=0):
+        tax_slab = frappe.db.get_value('Salary Structure Assignment', self.custom_salary_structure_assignment, 'income_tax_slab')
+        is_new_tax_slab = frappe.db.get_value('Income Tax Slab', tax_slab, 'custom_is_new_regime')
+
+        taxable_earnings = 0
+        additional_income = 0
+        additional_income_with_full_tax = 0
+        flexi_benefits = 0
+        amount_exempted_from_income_tax = 0
+
+        for earning in self.earnings:
+            if based_on_payment_days:
+                amount, additional_amount = self.get_amount_based_on_payment_days(earning)
+            else:
+                if earning.additional_amount:
+                    amount, additional_amount = earning.amount, earning.additional_amount
+                else:
+                    amount, additional_amount = earning.default_amount, earning.additional_amount
+
+            if earning.is_tax_applicable:
+                if earning.is_flexible_benefit:
+                    flexi_benefits += amount
+                else:
+                    taxable_earnings += amount - additional_amount
+                    additional_income += additional_amount
+
+                    if additional_amount and earning.is_recurring_additional_salary:
+                        additional_income += self.get_future_recurring_additional_amount(
+                            earning.additional_salary, earning.additional_amount
+                        )
+
+                    if earning.deduct_full_tax_on_selected_payroll_date:
+                        additional_income_with_full_tax += additional_amount
+
+        if allow_tax_exemption:
+            for ded in self.deductions:
+                if ded.exempted_from_income_tax:
+                    if is_new_tax_slab:
+                        not_exempt_for_new_tax_slab = frappe.db.get_value(
+                            'Salary Component', ded.salary_component, 'custom_do_not_exempt_for_new_tax_slab'
+                        )
+                        if not not_exempt_for_new_tax_slab:
+                            # Proceed only if not marked as "Do not exempt for new tax slab"
+                            pass
+                        else:
+                            continue  # Skip deduction if it's marked as not exempt in new slab
+
+                    # In old slab or allowed in new slab
+                    amount, additional_amount = ded.amount, ded.additional_amount
+                    if based_on_payment_days:
+                        amount, additional_amount = self.get_amount_based_on_payment_days(ded)
+
+                    taxable_earnings -= flt(amount - additional_amount)
+                    additional_income -= additional_amount
+                    amount_exempted_from_income_tax += flt(amount - additional_amount)
+
+                    if additional_amount and ded.is_recurring_additional_salary:
+                        additional_income -= self.get_future_recurring_additional_amount(
+                            ded.additional_salary, ded.additional_amount
+                        )
+
+        return frappe._dict(
+            {
+                "taxable_earnings": taxable_earnings,
+                "additional_income": additional_income,
+                "amount_exempted_from_income_tax": amount_exempted_from_income_tax,
+                "additional_income_with_full_tax": additional_income_with_full_tax,
+                "flexi_benefits": flexi_benefits,
+            }
+        )
+        
+    def set_salary_structure_assignment(self):
+        self._salary_structure_assignment = frappe.db.get_value(
+            "Salary Structure Assignment",
+            {
+                "employee": self.employee,
+                "salary_structure": self.salary_structure,
+                "from_date": ("<=", self.actual_start_date),
+                "docstatus": 1,
+            },
+            "*",
+            order_by="from_date desc",
+            as_dict=True,
+        )
+
+        if self._salary_structure_assignment:
+            self.custom_salary_structure_assignment = self._salary_structure_assignment.name
+
+        if not self._salary_structure_assignment:
+            frappe.throw(
+                _(
+                    "Please assign a Salary Structure for Employee {0} applicable from or before {1} first"
+                ).format(
+                    frappe.bold(self.employee_name),
+                    frappe.bold(formatdate(self.actual_start_date)),
+                )
+            )
+
+
