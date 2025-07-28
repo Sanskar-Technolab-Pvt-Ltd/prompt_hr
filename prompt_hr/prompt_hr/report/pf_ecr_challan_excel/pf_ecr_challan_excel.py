530d75fea53b87e08b012a688981ee35a9af61d2
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-28 15:46:40 +0530

    Modify PF ECR Challan Excel Report

--- prompt_hr/prompt_hr/report/pf_ecr_challan_excel/pf_ecr_challan_excel.py
+++ prompt_hr/prompt_hr/report/pf_ecr_challan_excel/pf_ecr_challan_excel.py
@@ -2,29 +2,25 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import get_datetime, getdate
+from frappe.utils import getdate, add_months
 import calendar
 
 def execute(filters=None):
-    month = frappe.utils.getdate(filters.get("month")).month
-    year = int(get_datetime().year)
-
-    # Set the start and end date for the selected month
-    from_date = getdate(f"{year}-{month:02d}-01")
-    last_day = calendar.monthrange(year, month)[1]
-    to_date = getdate(f"{year}-{month:02d}-{last_day}")
+    # Set the month and year from filters or default to current month
+    from_date = filters.get("from_date") or getdate()
+    to_date = filters.get("to_date") or add_months(from_date, 1)
 
     columns = [
         {"label": "UAN", "fieldname": "employee_number", "fieldtype": "Data", "width": 120},
         {"label": "MEMBER NAME", "fieldname": "employee_name", "fieldtype": "Data", "width": 180},
-        {"label": "GROSS WAGES", "fieldname": "gross_wages", "fieldtype": "Currency", "width": 120},
-        {"label": "EPF WAGES", "fieldname": "epf_wages", "fieldtype": "Currency", "width": 120},
-        {"label": "EPS WAGES", "fieldname": "eps_wages", "fieldtype": "Currency", "width": 120},
-        {"label": "EDLI WAGES", "fieldname": "edli_wages", "fieldtype": "Currency", "width": 120},
-        {"label": "EE SHARE REMITTED", "fieldname": "ee_share_remitted", "fieldtype": "Currency", "width": 150},
-        {"label": "EPS CONTRIBUTION REMITTED", "fieldname": "eps_contribution_remitted", "fieldtype": "Currency", "width": 180},
-        {"label": "ER SHARE REMITTED", "fieldname": "er_share_remitted", "fieldtype": "Currency", "width": 150},
-        {"label": "NCP DAYS", "fieldname": "ncp_days", "fieldtype": "Int", "width": 100},
+        {"label": "GROSS WAGES", "fieldname": "gross_wages", "fieldtype": "Currency", "width": 200},
+        {"label": "EPF WAGES", "fieldname": "epf_wages", "fieldtype": "Currency", "width": 200},
+        {"label": "EPS WAGES", "fieldname": "eps_wages", "fieldtype": "Currency", "width": 200},
+        {"label": "EDLI WAGES", "fieldname": "edli_wages", "fieldtype": "Currency", "width": 200},
+        {"label": "EE SHARE REMITTED", "fieldname": "ee_share_remitted", "fieldtype": "Currency", "width": 200},
+        {"label": "EPS CONTRIBUTION REMITTED", "fieldname": "eps_contribution_remitted", "fieldtype": "Currency", "width": 200},
+        {"label": "ER SHARE REMITTED", "fieldname": "er_share_remitted", "fieldtype": "Currency", "width": 200},
+        {"label": "NCP DAYS", "fieldname": "ncp_days", "fieldtype": "Int", "width": 120},
         {"label": "REFUNDS", "fieldname": "refund_of_advance", "fieldtype": "Data", "editable":1, "width": 150},
     ]
 
@@ -41,8 +37,8 @@ def execute(filters=None):
     }
 
     # Add employee filter only if present
-    if filters.get("employee"):
-        salary_filters["employee"] = filters.get("employee")
+    if filters.get("company"):
+        salary_filters["company"] = filters.get("company")
 
     salary_slips = frappe.get_all(
         "Salary Slip",
@@ -55,61 +51,43 @@ def execute(filters=None):
         salary_details = frappe.get_all(
             "Salary Detail",
             filters={"parent": slip.name},
-            fields=["salary_component", "amount"],
+            fields=["salary_component", "amount", "parentfield"],
 
         )
-        # house_rent_allowance = 0
-        # telephone_allowance = 0
-        # food_coupons = 0
-        # performance_bonus = 0
-        # lta = 0
-        # medical_allowance = 0
-        # internet_allowance  = 0
         basic = 0
         dearness_allowance = 0
+        provident_fund = 0
         if salary_details:
             for salary_detail in salary_details:
                 salary_comp = frappe.get_doc("Salary Component", salary_detail.salary_component)
-                # if salary_comp.custom_salary_component_type == "House Rent Allowance":
-                #     house_rent_allowance += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "LTA":
-                #     lta += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "Medical Allowance":
-                #     medical_allowance += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "Mobile & Internet Allowance":
-                #     internet_allowance += salary_detail.amount
                 if salary_comp.custom_salary_component_type == "Basic Salary":
                     basic += salary_detail.amount
                 elif salary_comp.custom_salary_component_type == "Dearness Allowance":
                     dearness_allowance += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "Food coupons Deduction":
-                #     food_coupons += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "Performance Bonus":
-                #     performance_bonus += salary_detail.amount
-                # elif salary_comp.custom_salary_component_type == "Telephone Reimbursement":
-                #     telephone_allowance += salary_detail.amount
+                elif salary_comp.custom_salary_component_type == "PF Employee Contribution" and salary_detail.parentfield=="deductions":
+                    provident_fund += salary_detail.amount
         
         epf_wages = basic + dearness_allowance
-        # eps_wages =  slip.gross_pay - lta - medical_allowance - internet_allowance - telephone_allowance - performance_bonus - food_coupons - house_rent_allowance
         if epf_wages > 15000:
             eps_wages = 15000
         else:
             eps_wages = epf_wages
         ee_share_remitted = epf_wages * 0.12
         eps_contribution_remitted = eps_wages * 0.0833
-        row = {
-            "employee_number": employee.custom_uan_number,
-            "employee_name": employee.employee_name,
-            "gross_wages": slip.gross_pay,
-            "epf_wages":epf_wages,
-            "eps_wages": eps_wages,
-            "edli_wages": epf_wages,
-            "ee_share_remitted": ee_share_remitted,
-            "eps_contribution_remitted":eps_contribution_remitted,
-            "er_share_remitted": ee_share_remitted - eps_contribution_remitted,
-            "ncp_days": slip.leave_without_pay,
-            "refund_of_advance": "",
-        }
-        data.append(row)
+        if provident_fund > 0:
+            row = {
+                "employee_number": employee.custom_uan_number,
+                "employee_name": slip.employee_name,
+                "gross_wages": slip.gross_pay,
+                "epf_wages":epf_wages,
+                "eps_wages": eps_wages,
+                "edli_wages": epf_wages,
+                "ee_share_remitted": ee_share_remitted,
+                "eps_contribution_remitted":eps_contribution_remitted,
+                "er_share_remitted": ee_share_remitted - eps_contribution_remitted,
+                "ncp_days": slip.leave_without_pay,
+                "refund_of_advance": 0,
+            }
+            data.append(row)
 
     return columns, data
