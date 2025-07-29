6ed0ea98684b444895c0361a075d0d40240630f7
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-29 11:54:19 +0530

    Modify PT Monthly Statement Report

--- prompt_hr/prompt_hr/report/pt_monthly_statement/pt_monthly_statement.js
+++ prompt_hr/prompt_hr/report/pt_monthly_statement/pt_monthly_statement.js
@@ -3,14 +3,48 @@
 
 frappe.query_reports["PT Monthly Statement"] = {
 	filters: [
-		{
-			fieldname: "month",
-			label: __("Month"),
-			fieldtype: "Select",
-			options: "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
-			default: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-			[frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()]
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.month_start()
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.month_end()
+        },
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            reqd: 0
+        }
+    ],
 
-		},
-	]
+	onload: function(report) {
+        const roles = frappe.user_roles || [];
+
+        // Button 1: Notify Accountify Team
+        if (roles.includes("HR User") || roles.includes("HR Manager")) {
+            report.page.add_inner_button("Notify Accountify Team", () => {
+                frappe.call({
+                    method: "prompt_hr.py.accounting_team_notifications.send_esic_challan_notification",
+                    args: {
+                        report_name: "PT Monthly Statement",
+                        url: window.location.href,
+                    },
+                    callback: function(r) {
+                        if (r.message === "success") {
+                            frappe.msgprint("Notification sent to the Accounting team Succesfully.");
+                        }
+                    }
+                });
+            }).removeClass("btn-default").addClass("btn-primary");
+        }
+	}
 };
