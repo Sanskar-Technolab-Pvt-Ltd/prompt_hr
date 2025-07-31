92a3366152afd87a1ecba3531b7da52f47fb91da
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-07-31 20:43:44 +0530

    Monthly MealCard Report

--- prompt_hr/prompt_hr/report/monthly_mealcard_report/monthly_mealcard_report.py
+++ prompt_hr/prompt_hr/report/monthly_mealcard_report/monthly_mealcard_report.py
@@ -5,87 +5,92 @@ import frappe
 from frappe.utils import getdate, format_date
 
 def execute(filters=None):
-	columns, data = get_columns(filters), get_data(filters)
-	return columns, data
+    columns, data = get_columns(filters), get_data(filters)
+    return columns, data
 
 def get_columns(filters):
-	return [
-    {
-        "label": "STAFF ID",
-        "fieldname": "employee",
-        "fieldtype": "Link",
-        "options": "Employee",
-        "width": 100,
-    },
-    {
-        "label": "Name",
-        "fieldname": "employee_name",
-        "fieldtype": "Data",
-        "width": 160,
-    },
-    {
-        "label": "Mobile Number",
-        "fieldname": "mobile_no",
-        "fieldtype": "Data",
-        "width": 130,
-    },
-    {
-        "label": "Corporate ID",
-        "fieldname": "corporate_id",
-        "fieldtype": "Data",
-        "width": 130,
-    },
-    {
-        "label": "Card No",
-        "fieldname": "meal_card_no",
-        "fieldtype": "Data",
-        "width": 130,
-    },
-    {
-        "label": "Ref No",
-        "fieldname": "meal_card_ref_no",
-        "fieldtype": "Data",
-        "width": 130,
-    },
-    {
-        "label": "Remarks",
-        "fieldname": "remarks",
-        "fieldtype": "Data",
-        "width": 140,
-    },
-    {
-        "label": "Handover On",
-        "fieldname": "handover_on",
-        "fieldtype": "Date",
-        "width": 120,
-    },
+    return [
         {
-        "label": "Meal Wallet Amount",
-        "fieldname": "meal_wallet_amount",
-        "fieldtype": "Currency",
-        "width": 160,
-    },
-    {
-        "label": "Fuel Wallet Amount",
-        "fieldname": "fuel_wallet_amount",
-        "fieldtype": "Currency",
-        "width": 160,
-    },
-    {
-        "label": "Attire Wallet Amount",
-        "fieldname": "attire_wallet_amount",
-        "fieldtype": "Currency",
-        "width": 180,
-    },
-]
-
-
+            "label": "STAFF ID",
+            "fieldname": "employee",
+            "fieldtype": "Link",
+            "options": "Employee",
+            "width": 100,
+        },
+        {
+            "label": "Name",
+            "fieldname": "employee_name",
+            "fieldtype": "Data",
+            "width": 160,
+        },
+        {
+            "label": "Mobile Number",
+            "fieldname": "mobile_no",
+            "fieldtype": "Data",
+            "width": 130,
+        },
+        {
+            "label": "Corporate ID",
+            "fieldname": "corporate_id",
+            "fieldtype": "Data",
+            "width": 130,
+        },
+        {
+            "label": "Card No",
+            "fieldname": "meal_card_no",
+            "fieldtype": "Data",
+            "width": 130,
+        },
+        {
+            "label": "Ref No",
+            "fieldname": "meal_card_ref_no",
+            "fieldtype": "Data",
+            "width": 130,
+        },
+        {
+            "label": "Remarks",
+            "fieldname": "remarks",
+            "fieldtype": "Data",
+            "width": 140,
+        },
+        {
+            "label": "Handover On",
+            "fieldname": "handover_on",
+            "fieldtype": "Date",
+            "width": 120,
+        },
+        {
+            "label": "Meal Wallet Amount",
+            "fieldname": "meal_wallet_amount",
+            "fieldtype": "Currency",
+            "width": 160,
+        },
+        {
+            "label": "Fuel Wallet Amount",
+            "fieldname": "fuel_wallet_amount",
+            "fieldtype": "Currency",
+            "width": 160,
+        },
+        {
+            "label": "Attire Wallet Amount",
+            "fieldname": "attire_wallet_amount",
+            "fieldtype": "Currency",
+            "width": 180,
+        },
+        {
+            "label": "Telecom Wallet Amount",
+            "fieldname": "telecom_wallet_amount",
+            "fieldtype": "Currency",
+            "width": 200,
+        }
+    ]
 
 def get_data(filters):
-	# Set the start and end date for the selected month
+    # Set the start and end date for the selected month
     from_date = getdate(filters.get("from_date"))
     to_date = getdate(filters.get("to_date"))
     corporate_id_for_meal_card = frappe.db.get_single_value("HR Settings", "custom_corporate_id_for_meal_card")
+
     # Filters for Salary Slips
     salary_slip_filters = {
         "docstatus": 1,
@@ -108,25 +113,44 @@ def get_data(filters):
         employee = frappe.get_doc("Employee", slip.employee)
         salary_details = frappe.get_all(
             "Salary Detail",
-            filters={"parent": slip.name, "parentfield":"earnings"},
-            fields=["salary_component", "amount"],
-
+            filters={
+                "parent": slip.name,
+                "parentfield": "earnings"
+            },
+            fields=["salary_component", "amount"]
         )
+
         meal_amount = 0
         fuel_amount = 0
         attire_amount = 0
+        telecom_amount = 0
+
         for detail in salary_details:
             salary_comp = frappe.get_doc("Salary Component", detail.salary_component)
             if salary_comp.custom_salary_component_type == "Petrol Allowance":
                 fuel_amount += detail.amount
             elif salary_comp.custom_salary_component_type == "Professional Attire & Development Allowance":
-            	attire_amount+= detail.amount
+                attire_amount += detail.amount
             elif salary_comp.custom_salary_component_type == "Meal Coupon":
-            	meal_amount += detail.amount
+                meal_amount += detail.amount
+            elif salary_comp.custom_salary_component_type == "Mobile & Internet Allowance":
+                telecom_amount += detail.amount
 
+        if (
+            employee.status == "Active"
+            and (
+                employee.custom_mobile_and_internet_card_consent
+                or employee.custom_fuel_card_consent
+                or employee.custom_attire_card_consent
+                or employee.custom_meal_card_consent
+            )
+        ):
+            mobile_no = (
+                employee.cell_number
+                if employee.custom_preferred_mobile == "Personal Mobile No"
+                else employee.custom_work_mobile_no
+            )
 
-        if employee.status=="Active" and (employee.custom_telephone_reimbursement_applicable or employee.custom_fuel_card_consent or employee.custom_attire_card_consent or employee.custom_meal_card_consent):
-            mobile_no = employee.cell_number if employee.custom_preferred_mobile == "Personal Mobile No" else employee.custom_work_mobile_no
             row = {
                 "employee": slip.employee,
                 "employee_name": slip.employee_name,
@@ -137,8 +161,9 @@ def get_data(filters):
                 "remarks": "",
                 "handover_on": "",
                 "attire_wallet_amount": attire_amount,
-                "fuel_wallet_amount":fuel_amount,
-                "meal_wallet_amount":meal_amount,
+                "fuel_wallet_amount": fuel_amount,
+                "meal_wallet_amount": meal_amount,
+                "telecom_wallet_amount": telecom_amount
             }
             data.append(row)
 
