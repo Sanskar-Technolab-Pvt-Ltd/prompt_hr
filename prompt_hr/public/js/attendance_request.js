e5530d91b8d5fad16a26221473cb68e964c700e3
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-08-26 12:02:08 +0530

    Attendance Request and Regularization js code update

--- prompt_hr/public/js/attendance_request.js
+++ prompt_hr/public/js/attendance_request.js
@@ -21,28 +21,28 @@ frappe.ui.form.on('Attendance Request', {
     },
 
     refresh: function (frm) {
-        if (frm.doc.employee) {
-            frappe.call({
-                method: "prompt_hr.py.utils.check_user_is_reporting_manager",
-                args: {
-                    user_id: frappe.session.user,
-                    requesting_employee_id: frm.doc.employee
-                },
-                callback: function (res) {
-                    if (!res.message.error) {
-                        if (res.message.is_rh) {
-                            frm.fields.filter(field => field.has_input).forEach(field => {
-                                frm.set_df_property(field.df.fieldname, "read_only", 1);
-                            });
-                            frm.set_df_property("custom_status", "hidden", 0)
-                        }
+        // if (frm.doc.employee) {
+        //     frappe.call({
+        //         method: "prompt_hr.py.utils.check_user_is_reporting_manager",
+        //         args: {
+        //             user_id: frappe.session.user,
+        //             requesting_employee_id: frm.doc.employee
+        //         },
+        //         callback: function (res) {
+        //             if (!res.message.error) {
+        //                 if (res.message.is_rh) {
+        //                     frm.fields.filter(field => field.has_input).forEach(field => {
+        //                         frm.set_df_property(field.df.fieldname, "read_only", 1);
+        //                     });
+        //                     frm.set_df_property("custom_status", "hidden", 0)
+        //                 }
 
-                    } else if (res.message.error) {
-                        frappe.throw(res.message.message)
-                    }
-                }
-            })
-        }
+        //             } else if (res.message.error) {
+        //                 frappe.throw(res.message.message)
+        //             }
+        //         }
+        //     })
+        // }
 
         if (frm.doc.company) {
             set_partial_day_option(frm)
