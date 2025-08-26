4c31d3b4856a46c1e771b4590b60600a7651ebad
Vanparia-Ayush <ayush@sanskartechnolab.com>
2025-08-26 12:02:08 +0530

    Attendance Request and Regularization js code update

--- prompt_hr/prompt_hr/doctype/attendance_regularization/attendance_regularization.js
+++ prompt_hr/prompt_hr/doctype/attendance_regularization/attendance_regularization.js
@@ -1,30 +1,8 @@
 // Copyright (c) 2025, Jignasha Chavda and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Attendance Regularization", {
-	refresh(frm) {
-        if (frm.doc.employee){
-			frappe.call({
-                method: "prompt_hr.py.utils.check_user_is_reporting_manager",
-                args: {
-                    user_id: frappe.session.user,
-                    requesting_employee_id: frm.doc.employee
-                },
-                callback: function (res) {
-                    if (!res.message.error) {
-                        if (res.message.is_rh) {
-                            frm.set_df_property("status", "hidden", 0);
-                            if (!has_common(frappe.user_roles, ["S - HR Director (Global Admin)", "System Manager"]))
-								frm.fields.filter(field => field.has_input).forEach(field => {
-                                    frm.set_df_property(field.df.fieldname, "read_only", 1);
-                                });
-                        }
-
-                    } else if (res.message.error) {
-                        frappe.throw(res.message.message)
-                    }
-                }
-            })
-		}
-    }
-});
+// frappe.ui.form.on("Attendance Regularization", {
+// 	refresh(frm) {
+        
+//     }
+// });
