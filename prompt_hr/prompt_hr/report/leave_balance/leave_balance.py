76c97f18bbf87e3b40b624bee6c07fcb04075ae4
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-09-22 11:30:42 +0530

    Leave Modification For Deduct Leave button

--- prompt_hr/prompt_hr/report/leave_balance/leave_balance.py
+++ prompt_hr/prompt_hr/report/leave_balance/leave_balance.py
@@ -5,9 +5,7 @@ from itertools import groupby
 
 
 from frappe import _
-from prompt_hr.py.leave_application import custom_get_data
-
-
+from prompt_hr.py.leave_application import custom_get_data, custom_get_columns
 import frappe
 
 
@@ -18,69 +16,10 @@ def execute(filters: Filters | None = None) -> tuple:
 	if filters.to_date <= filters.from_date:
 		frappe.throw(_('"From Date" can not be greater than or equal to "To Date"'))
 
-	columns = get_columns()
+	columns = custom_get_columns()
 	data = custom_get_data(filters)
 	charts = get_chart_data(data, filters)
 	return columns, data, None, charts
-
-
-
-def get_columns() -> list[dict]:
-	return [
-		{
-			"label": _("Leave Type"),
-			"fieldtype": "Link",
-			"fieldname": "leave_type",
-			"width": 200,
-			"options": "Leave Type",
-		},
-		{
-			"label": _("Employee"),
-			"fieldtype": "Link",
-			"fieldname": "employee",
-			"width": 100,
-			"options": "Employee",
-		},
-		{
-			"label": _("Employee Name"),
-			"fieldtype": "Dynamic Link",
-			"fieldname": "employee_name",
-			"width": 100,
-			"options": "employee",
-		},
-		{
-			"label": _("Opening Balance"),
-			"fieldtype": "float",
-			"fieldname": "opening_balance",
-			"width": 150,
-		},
-		{
-			"label": _("New Leave(s) Allocated"),
-			"fieldtype": "float",
-			"fieldname": "leaves_allocated",
-			"width": 200,
-		},
-		{
-			"label": _("Leave(s) Taken"),
-			"fieldtype": "float",
-			"fieldname": "leaves_taken",
-			"width": 150,
-		},
-		{
-			"label": _("Leave(s) Expired"),
-			"fieldtype": "float",
-			"fieldname": "leaves_expired",
-			"width": 150,
-		},
-		{
-			"label": _("Closing Balance"),
-			"fieldtype": "float",
-			"fieldname": "closing_balance",
-			"width": 150,
-		},
-	]
-
-
 
 def get_chart_data(data: list, filters: Filters) -> dict:
 	labels = []
@@ -118,4 +57,3 @@ def get_dataset_for_chart(employee_data: list, datasets: list, labels: list) -> 
 
 	for leave in leaves:
 		datasets.append({"name": leave.leave_type, "values": [leave.closing_balance]})
-
