56b3ebab4a771da86a31568916ad24eab45dc34d
Vanparia-Ayush <ayushvanparia@sanskartechnolab.com>
2025-11-02 19:06:02 +0530

    Timesheet Changes

--- prompt_hr/public/js/timesheet.js
+++ prompt_hr/public/js/timesheet.js
@@ -10,13 +10,90 @@ frappe.ui.form.on("Timesheet", {
 				},
 			};
 		};
+    },
+    refresh(frm) {
+        if (frm.doc.status === "Submitted" || frm.doc.status === "Requested for Unsubmission") {
+            // Disable all fields
+            frm.fields_dict && Object.keys(frm.fields_dict).forEach(fieldname => {
+
+                frm.set_df_property(fieldname, "read_only", 1);
+            });
+
+            frm.fields_dict.timesheet_details?.grid?.df?.fields?.forEach(field => {
+                frm.fields_dict.timesheet_details.grid.set_column_disp(field.fieldname, false);
+            });
+
+        } else {
+            frm.fields_dict && Object.keys(frm.fields_dict).forEach(fieldname => {
+                if (!["naming_series", "user", "start_date", "end_date", "employee_name", "department", "total_hours", "total_billable_hours", "base_total_billable_amount", "base_total_billed_amount", "base_total_costing_amount", "total_billed_hours", "total_billable_amount", "total_billed_amount", "total_costing_amount", "per_billed", "sales_invoice", "salary_slip", "status"].includes(fieldname)) {
+                        frm.set_df_property(fieldname, "read_only", 0);
+                }
+            });
+
+        }
+
+    },
+    // before_workflow_action: async (frm) => {
+    //     const allowed_actions = ['Submit', 'Request for Unsubmit Timesheet'];
+    //     const action = frm.selected_workflow_action;
+
+    //     if (allowed_actions.includes(action)){
+    //         let promise = new Promise ((resolve, reject) => {
+    //             frappe.dom.unfreeze()
+
+    //             const timesheet_date = frm.doc.date;
+    //             const weeks = frappe.get_doc('HR Settings', null)?.custom_allowed_weeks_to_unsubmit_timesheet_for_prompt || 5;
+    //             const cutoff = frappe.datetime.add_days(frappe.datetime.now_date(), -(weeks * 7));
+    //             console.log("asdasdsa", cutoff)
+    //             const is_valid = frappe.datetime.str_to_obj(timesheet_date) >= frappe.datetime.str_to_obj(cutoff);
+
+    //             if (!is_valid) {
+                    
+    //                 frappe.msgprint({
+    //                     title: __('Timesheet Too Old'),
+    //                     message: __('You can only {0} timesheets from the last {1} week(s). This timesheet is older than allowed.',[ action === 'submit' ? 'submit' : 'request unsubmit', weeks]), indicator: 'red'});
+    //                 reject()
+                    
+    //             }
+    //             else{
+    //                 resolve()
+    //             }
+    //         })
+
+    //         await promise
+    //     }
+    // }
+
+    before_workflow_action: async (frm) => {
+        const allowed_actions = ['Submit', 'Request for Unsubmit Timesheet'];
+        const action = frm.selected_workflow_action;
+
+        if (allowed_actions.includes(action)) {
+            frappe.dom.unfreeze();
+
+            const weeks = await frappe.db.get_single_value('HR Settings', 'custom_allowed_weeks_to_unsubmit_timesheet_for_prompt');
+            const allowed_weeks = weeks || 5;
+
+            const timesheet_date = frm.doc.custom_date;
+            const cutoff = frappe.datetime.add_days(frappe.datetime.now_date(), -(allowed_weeks * 7));
+            const is_valid = frappe.datetime.str_to_obj(timesheet_date) >= frappe.datetime.str_to_obj(cutoff);
+            
+            console.log("asdasdas", is_valid, cutoff, timesheet_date)
+            if (!is_valid) {
+                frappe.throw({
+                    title: __('Timesheet Too Old'),
+                    message: __(`You can only ${action === 'Submit' ? 'submit' : 'request unsubmit'} timesheets from the last ${allowed_weeks} week(s). This timesheet is older than allowed.`)
+                });
+            }
+        }
     }
+
 })
 
 frappe.ui.form.on("Timesheet Detail", {
     form_render: function(frm, cdt, cdn){
         let row = locals[cdt][cdn]
-        field_names = ["is_billable", "billing_hours", "billing_rate", "billing_amount", "costing_rate", "costing_amount"]
+        field_names = ["is_billable", "billing_hours", "billing_rate", "billing_amount", "costing_rate", "costing_amount", "base_billing_rate", "base_billing_amount", "base_costing_rate", "base_costing_amount"]
         
 
             frappe.call({
@@ -45,6 +122,48 @@ frappe.ui.form.on("Timesheet Detail", {
                 }
             })
         
+        
+    },
+
+    time_logs_add: function(frm, cdt,cdn){
+        let row = locals[cdt][cdn]
+
+        frappe.call({
+            method: "prompt_hr.py.timesheet.set_billing_rate",
+            args: {
+                doc: frm.doc
+            },
+            callback: function(r){
+                if (r.message && r.message.billing_rate){
+                    row.billing_rate = r.message.billing_rate   
+                }
+            }
+        })
+        
+    },
+
+    is_billable: function(frm, cdt, cdn){
+
+        let row = locals[cdt][cdn]
+        
+        if (row.is_billable){
+            console.log("Called")
+            frappe.call({
+                method: "prompt_hr.py.timesheet.set_billing_rate",
+                args: {
+                    doc: frm.doc
+                },
+                callback: function(r){
+                    console.log("res", r)
+                    if (r.message && r.message.billing_rate){
+
+                        row.billing_rate = r.message.billing_rate   
+                    }
+
+                    frm.refresh_field("time_logs")
+                }
+            })
+        }
         
     }
 })
--- prompt_hr/py/timesheet.py
+++ prompt_hr/py/timesheet.py
@@ -1,4 +1,61 @@
 import frappe
+from frappe import _
+from frappe.utils import getdate, add_days, today, cint
+from datetime import datetime
+
+
+
+
+@frappe.whitelist()
+def set_billing_rate(doc):
+    
+    doc = frappe.parse_json(doc)
+    billing_rate = 0        
+    monthly_salary = 0
+    
+        
+    gross_salary = frappe.db.get_value("Employee", doc.employee, "custom_gross_salary")
+    ctc_salary = frappe.db.get_value("Employee", doc.employee, "ctc") 
+    
+    if gross_salary:
+        monthly_salary = gross_salary
+    else:
+        monthly_salary = ctc_salary
+    
+    working_days = frappe.db.get_single_value("HR Settings", "custom_working_days_to_calculate_billing_rate_for_prompt") or 28
+    daily_salary = 0
+    
+    shift = frappe.db.get_value("Employee", doc.employee, "default_shift")
+    shift_hours = 0
+    break_hours = 0
+    if shift:
+        shift_data = frappe.db.get_values("Shift Type", shift, ["start_time", "end_time", "custom_break_time"], as_dict=True) 
+        
+    
+    if shift_data:
+        shift_data = shift_data[0]
+        if shift_data.get("start_time") and shift_data.get("end_time"):
+            fmt = "%H:%M:%S"
+            start = datetime.strptime(str(shift_data.get("start_time")), fmt)
+            end = datetime.strptime(str(shift_data.get("end_time")), fmt)
+            
+            total_hours = (end - start).total_seconds() / 3600
+            if shift_data.get("custom_break_time"):
+                break_hours = ( shift_data.get("custom_break_time") or 0) / 60
+                
+            shift_hours = total_hours - break_hours
+                    
+    if gross_salary:
+        daily_salary = float(monthly_salary / working_days)
+    
+    
+    if daily_salary and shift_hours:    
+        billing_rate = float(daily_salary / shift_hours)
+
+        
+    return {"billing_rate": billing_rate}
+
+
 
 # * IF THE USER IS PROJEC CO-ORDINATOR OR HAS ANY OF THE HR MANAGER ROLES THEN RETURNING 1 ELSE O
 @frappe.whitelist()
@@ -18,7 +75,11 @@ def show_filds_for(project = None):
     except Exception as e:
         frappe.log_error("error_show_fields_for", frappe.get_traceback())
         return {"error": 1, "message": str(e), "show_fields": 0}
-    
+         
 
-
+def is_within_allowed_weeks(self, weeks: int) -> bool:
+        """
+        Return True if start_date >= today - (weeks * 7) days
+        """
+        
 
